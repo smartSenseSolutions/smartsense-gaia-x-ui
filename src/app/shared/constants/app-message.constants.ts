16d322a72f3b71e556c7b6ffe77f34006f54818e
Mukund Sonaiya <mukund.sonaiya@smartsensesolutions.com>
2023-04-20 17:56:27 +0530

    Handle api error messages

--- src/app/shared/constants/app-message.constants.ts
+++ src/app/shared/constants/app-message.constants.ts
@@ -1,4 +1,7 @@
 export class AppMessageConstants {
+  public static readonly NO_INTERNET_CONNECTION = 'No internet connection';
+  public static readonly UNAUTHORIZED =
+    'You are not authorized to access this feature.';
   public static readonly SESSION_MSG =
     'Session has expired! Please login again.';
 }
