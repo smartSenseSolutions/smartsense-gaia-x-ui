80f2599ee6b2dae39a89d9eb2f5ac34d67be14d7
Vaibhavi <vaibhavi.ganatra@smartsensesolutions.com>
2023-04-20 17:05:26 +0530

    console error resolved for share info dialog

--- src/app/shared/components/share-information-dialog/share-information-dialog.component.ts
+++ src/app/shared/components/share-information-dialog/share-information-dialog.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
@@ -5,7 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-share-information-dialog',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './share-information-dialog.component.html',
   styleUrls: ['./share-information-dialog.component.scss'],
 })
