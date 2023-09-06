779798864f3bf05f9ea506a7f5970a1b3c970130
Pinank Lakhani <pinank.lakhani@smartsensesolutions.com>
2023-09-06 15:51:37 +0530

    fix: label level vc json added in frontend

--- src/app/private/models/add-service-offer-request.model.ts
+++ src/app/private/models/add-service-offer-request.model.ts
@@ -1,6 +1,9 @@
 import { AddServiceDataModel } from "./add-service-data.model";
 import { AddServiceModel } from "./add-service.model";
+import { LabelLevelModel } from './label-level.model';
 
-export interface AddServiceOfferRequest extends AddServiceModel, AddServiceDataModel{
-
+export interface AddServiceOfferRequest
+  extends AddServiceModel,
+    AddServiceDataModel {
+  labelLevel: any;
 }
