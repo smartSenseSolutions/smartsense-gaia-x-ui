d21d7bbc11ee484b2365b942430817eb9fd5a297
MihirModi1410 <mihir.modi@smartsensesolutions.com>
2023-12-06 11:16:56 +0530

    Dynamic Domain Logs added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,11 @@ export class AppComponent {
 
   //  Life cycle methods
   ngOnInit() {
+    console.log('Host: ', window.location.host);
+    const domain = window.location.host.split('gaiaxui.')[1];
+    console.log('Domain: ', domain);
+    console.log('URL => ', `https://gaiaxapi.${domain}`);
+
     this.initialize();
   }
 
