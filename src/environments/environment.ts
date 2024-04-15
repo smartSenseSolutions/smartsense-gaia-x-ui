edcf002bc67dee06be774ff0563b7075e0e09329
Ronak Thacker <ronak.thacker@smartsensesolutions.com>
2024-04-15 18:00:07 +0530

    fix: updated cred def id

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   BASE_URL: 'https://gaiaxapi.proofsense.in',
   OCM_URL: 'https://gaiax-ocm-engine.proofsense.in',
-  CRED_DEF_MEMBER: 'GEQmh5nhnJMHpgznYddbbi:3:CL:208992:smart-x-member-Credentials-Defs',
+  CRED_DEF_MEMBER: 'GEQmh5nhnJMHpgznYddbbi:3:CL:600902:smart-x-login-creds-defs',
   CRED_DEF_ID_PARTICIPANT:
-    'GEQmh5nhnJMHpgznYddbbi:3:CL:208997:smart-x-legal-participant-Credentials-Defs',
+    'GEQmh5nhnJMHpgznYddbbi:3:CL:600909:smart-x-legal-participant-creds-defs',
 };
