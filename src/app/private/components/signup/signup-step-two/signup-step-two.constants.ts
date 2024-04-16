d55cb5925fced85ed254181d41258833b940858c
Viraj Patva <viraj.patva@smartsensesolutions.com>
2024-04-16 12:09:24 +0530

    added new locations

--- src/app/private/components/signup/signup-step-two/signup-step-two.constants.ts
+++ src/app/private/components/signup/signup-step-two/signup-step-two.constants.ts
@@ -3,5 +3,13 @@ export const REGISTRATION_TYPES = [
   { label: 'EORI', value: 'EORI' },
   { label: 'LEI Code', value: 'leiCode' },
 ];
-
-export const COUNTRY_SUBDIVISION_CODES = [{ label: 'BE-BRU', value: 'BE-BRU' }];
+//DE-BE, AD-02, AF-Kan, AL-11, AO-CUS, AR-P
+export const COUNTRY_SUBDIVISION_CODES = [
+  { label: 'DE-BE', value: 'DE-BE' },
+  { label: 'AD-02', value: 'AD-02' },
+  { label: 'AF-Kan', value: 'AF-Kan' },
+  { label: 'AL-11', value: 'AL-11' },
+  { label: 'AO-CUS', value: 'AO-CUS' },
+  { label: 'AR-P', value: 'AR-P' },
+  { label: 'BE-BRU', value: 'BE-BRU' },
+];
